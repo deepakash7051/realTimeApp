1d9c387660aa4ae4307eadd9da5a96239c4056b6
Your Name <deepakash7051@gmail.com>
2020-09-04 11:17:28 +0530

    DB:Relations, DB:Factory & Seeds, apiRoute

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Like;
+use Faker\Generator as Faker;
+use App\User;
+
+$factory->define(Like::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return User::all()->random();
+        }
+    ];
+});
