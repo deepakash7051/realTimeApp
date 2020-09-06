e9d59eee516ba6bf6abeec718de5a984c5f17217
Your Name <deepakash7051@gmail.com>
2020-09-06 13:04:23 +0530

    Like and Dislike System for Reply

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::apiResource('/question', 'QuestionController');
+
 Route::apiResource('/category', 'CategoryController');
+
 Route::apiResource('/question/{question}/reply','ReplyController');
+
+Route::post('/like/{reply}','LikeController@likeIt');
+
+Route::delete('/like/{reply}','LikeController@unLikeIt');
